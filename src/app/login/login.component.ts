b276d95e80752832c9d4192ef06be2564cf05a4d
Vetso <vetso.nombana@gmail.com>
2021-03-27 18:34:25 +0000

    interface login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
+
+export class CheckUser {
+  loginError = "Login Failed: Your email or password is incorrect"
+  state = false;
+}
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  checkUser: CheckUser = new CheckUser();
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  
+
+  constructor(private authService:AuthService, private router:Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+
+
+  doLogin() {
+    // si je suis pas loggé, je me loggue, sinon, si je suis
+    // loggé je me déloggue et j'affiche la page d'accueil
+    if((!this.email.value) || (!this.password.value)) return;
+   /* if(this.authService.loggedIn) {
+      // je suis loggé
+      // et bien on se déloggue
+      this.authService.logOut();
+      // on navigue vers la page d'accueil
+      this.router.navigate(["/home"]);
+    } else {*/
+      // je ne suis pas loggé, je me loggue
+      this.authService.logIn(this.email.value, this.password.value).subscribe((data) => {
+        if(data.auth) this.router.navigate(["/assignments"]);
+      }, (error) => {
+        this.checkUser.state = true;
+      });
+  }
+
+
+
+ 
+}
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
@@ -7,16 +10,29 @@ export class AuthService {
   loggedIn = false;
   admin = false;
 
-  constructor() {}
+  constructor(private http:HttpClient) {}
 
   logIn(login, password) {
     // typiquement, acceptera en paramètres un login et un password
     // vérifier qu'ils sont ok, et si oui, positionner la propriété loggedIn à true
     // si login/password non valides, positionner à false;
 
-    if (login === 'admin') this.admin = true;
+    /*if (login === 'admin') this.admin = true;
 
-    this.loggedIn = true;
+    this.loggedIn = true;*/
+
+    const user = {
+      email:login,
+      password:password
+    }
+    
+    const headers = { 'Content-Type': 'application/json'}  ;
+    const body = JSON.stringify(user);
+    return this.http.post<any>('http://localhost:8010/api/login',body, { 'headers': headers })
+        .pipe(
+            retry(3),
+            catchError(this.handleError) //fonction mandray fonction
+        );
   }
 
   logOut() {
@@ -31,4 +47,26 @@ export class AuthService {
       resolve(this.admin);
     });
   }
+
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+        console.error('An error occurred:', error.error.message);
+    } else {
+        console.error(
+            `Backend returned code ${error.status}, ` +
+            `body was: ${error.error}`);
+    }
+    return throwError(
+        'Something bad happened; please try again later.');
+  };
+
+ /* private handleError<T>(operation: any, result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error); // pour afficher dans la console
+      console.log(operation + ' a échoué ' + error.message);
+
+      return of(result as T);
+    };
+  }*/
 }
