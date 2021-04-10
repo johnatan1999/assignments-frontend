ee0b9a1a98cc09e1a5d99afdd1ad861e77a6c235
johnatan <nathanjoh143@gmail.com>
2021-04-11 00:21:09 +0400

    active access token

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>,
+              next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            const token = user.token;
+            const cloned = req.clone({
+                headers: req.headers.set("Authorization",
+                    "Bearer " + token)
+            });
+
+            return next.handle(cloned);
+        }
+        else {
+            return next.handle(req);
+        }
+    }
+}
--- src/app/shared/services/basic.service.ts
+++ src/app/shared/services/basic.service.ts
@@ -1,14 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { LoggingService } from "./login.service";
+
 export class BasicService {
 
     static base_uri = "http://localhost:8010/api";
     // uri = "https://assignments-backend.herokuapp.com/api/assignments"
   // base_uri = "https://assignments-backend.herokuapp.com/api/"
+
+    // constructor(protected loggingService:LoggingService, 
+    //             protected http:HttpClient) { }
     
     getUri(path) {
         return `${BasicService.base_uri}${path}`.replace(/\//g, '/');
     }
 
+
+
     generateId():number {
       return Math.round(Math.random()*100000);
     }
+
 }
